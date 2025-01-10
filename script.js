0907ae1d610985980c9a801552d4df93d8e0c031
Nitsach1 <nitsach.rrhh@gmail.com>
2025-01-10 08:08:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function redirectToWhatsApp(productId) {
     { id: 2, name: "Mario theft auto", price: 80.00 },
     { id: 3, name: "Mario Samurai", price: 80.00 },
     { id: 4, name: "Blooming 💙", price: 120.00 },
-    { id: 5, name: "Polera Personalizadas ✨", price: Cotizar el precio }
+    { id: 5, name: "Polera Personalizadas ✨", price: cuanto lo hace }
   ];
 
   // Buscar el producto por su ID
